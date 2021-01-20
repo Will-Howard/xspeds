d295be80d95d09621b77aa6db01f27bfab9df9ac
William Howard <whoward@cmdty.com>
2021-01-20 15:31:35 +0000

    somewhat working mock data generator using the new Spectrum class

--- mock_data.py
+++ mock_data.py
@@ -1,6 +1,8 @@
 # TODO probably rename this, or put it in its own module
+from xspeds.spectrum import Spectrum
 from xspeds.constants import IMAGE_HEIGHT, IMAGE_WIDTH
 import numpy as np
+
 
 def gen_image(spectrum, center, boundary) -> np.ndarray:
     """Simple function to generate an image from a spectrum given
@@ -29,6 +31,60 @@ def gen_image(spectrum, center, boundary) -> np.ndarray:
         displacement = (l/_max_l)*boundary
         phi = np.random.uniform(0, 2*np.pi)
 
-        image[int(center[0] + displacement*np.cos(phi))][int(center[1] + displacement*np.sin(phi))] += 1
+        image[int(center[0] + displacement*np.cos(phi))
+              ][int(center[1] + displacement*np.sin(phi))] += 1
+
+    return image
+
+
+def gen_image2(spectrum: Spectrum, x_displacement, y_displacement, x_width, y_width, x_pixels=IMAGE_WIDTH, y_pixels=IMAGE_HEIGHT) -> np.ndarray:
+    """Generate a diffraction pattern from a spectrum.
+    For now assume the image plane is normal to the axis of the crystal planes,
+    the semiangle of the diffraction cone for a particular wavelentght is delta = 90 - theta
+    where theta is the Bragg angle, so the radius of the ring in the image plane is
+    R = L*tan(delta), where L is the distance to the crystal.
+    x_displacement, y_displacement, x_width, y_width are all in units of L
+    (and should therefore be quite small)
+
+    NOTE: in a general experimental setup the image plane will not be aligned
+    with the crystal planes, and the pattern for a particular wavelength on the
+    image will be a general conic section
+    TODO implement a more general setup
+
+    Args:
+        spectrum (Spectrum): The spectrum being diffracted throught the crystal
+        x_displacement ([type]): x displacment of top left corner of detector
+        y_displacement ([type]): x displacment of top left corner of detector # TODO make sure this is right
+        x_width ([type]): width of the detector
+        y_width ([type]): height of the detector
+
+    Returns:
+        np.ndarray: [description]
+    """
+    image = np.zeros((IMAGE_WIDTH, IMAGE_HEIGHT))
+    x_pixel_conversion = x_pixels/x_width
+    y_pixel_conversion = y_pixels/y_width
+
+    # TODO restrict the range of phi to speed things up
+    # corner_coords = [(x_displacement, y_displacement), (x_displacement, y_displacement + y_width),
+    #                  (x_displacement + x_width, y_displacement), (x_displacement + x_width, y_displacement + y_width)]
+    # corner_phis = list(map(lambda p: np.angle(p[0] + p[1]*1j, corner_coords)))
+
+    # number of points should depend on intensity
+    for _lambda in spectrum.random_sample(int(spectrum.total_intensity*100)):
+        # TODO do the displaecment correctly (using bragg's law)
+        c_delta = _lambda/2  # cos(delta), _lambda is in units of d
+        R = np.sqrt(1/(c_delta**2) - 1)
+
+        phi = np.random.uniform(-np.pi, np.pi)
+        x_image, y_image = R * \
+            np.cos(phi) - x_displacement, R*np.sin(phi) - y_displacement
+
+        # check if sample hits detector
+        if 0 < x_image < x_width and 0 < y_image < y_width:
+            x_pixel = int(x_image * x_pixel_conversion)
+            y_pixel = int(y_image * y_pixel_conversion)
+            # TODO make this a separate function and do clustering
+            image[x_pixel][y_pixel] += 1
 
     return image
--- spectrum.py
+++ spectrum.py
@@ -1,9 +1,11 @@
+from typing import List, Tuple
 import numpy as np
 from scipy import integrate
 
 class Spectrum:  # TODO is there any benefit to signalling it's abstractness in some way (with a decorator)
     """Abstract class representing a spectrum of radiation
     """
+    total_intensity = 0
 
     def random_sample(self, n: int):
         """Randomly select n wavelengths from the spectrum
@@ -11,7 +13,7 @@ class Spectrum:  # TODO is there any benefit to signalling it's abstractness in 
         Returns:
             generator: [description]
         """
-        return (self.inv_intensity_cdf(np.random.uniform) for _ in range(n))
+        return (self.inv_cdf(np.random.uniform()) for _ in range(n))
         
     def intensity(self, _lambda):
         pass
@@ -19,20 +21,22 @@ class Spectrum:  # TODO is there any benefit to signalling it's abstractness in 
     def cdf(self, _lambda):
         pass
 
-    def inv_cdf(self, _lambda):
+    def inv_cdf(self, percentile):
         pass
 
 class InterpolatedSpectrum(Spectrum):
-    """Simple spectrum class that interpolates from an array of intensities
+    """Simple spectrum class that interpolates from a list of intensity points
+    TODO the pdf and cdf don't quite correspond but this is good enough for now
     """
 
-    def __init__(self, _lambdas, intensities):
-        self._lambdas = np.array(_lambdas)
+    def __init__(self, points: List[Tuple]):
+        sorted_points = sorted(points, key=lambda p: p[0]) # sort by wavelength
+        self._lambdas = np.array([p[0] for p in sorted_points])
+        self._intensities = np.array([p[1] for p in sorted_points])
         self._min = self._lambdas[0]
         self._max = self._lambdas[-1]
-        self._intensities = np.array(intensities)
 
-        self.total_intensity = np.trapz(self._intensities, self._lambdas)
+        self.total_intensity = np.trapz(self._intensities, self._lambdas)  # TODO not sure about this
 
         # cache values of the cdf so it can be inverted quickly
         # TODO there is a better way to do this
@@ -40,18 +44,18 @@ class InterpolatedSpectrum(Spectrum):
         self.cached_cdf = [self.cdf(l) for l in self.cached_lambdas]
 
 
-    def intensity_df(self, _lambda):
+    def intensity(self, _lambda):
         return np.interp(_lambda, self._lambdas, self._intensities)
 
     def pdf(self, _lambda):
-        return self.intensity_df(_lambda)/self.total_intensity
+        return self.intensity(_lambda)/self.total_intensity
 
     def cdf(self, _lambda):
         if _lambda < self._min:
             return 0.0
         elif _lambda > self._max:
             return 1.0
-        return integrate.quad(self.pdf, self._lambdas[0], _lambda)[0]
+        return integrate.quad(self.pdf, self._min, _lambda, points=self._lambdas)[0]
 
     def inv_cdf(self, p):
         return np.interp(p, self.cached_cdf, self.cached_lambdas)
